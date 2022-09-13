cc3dc5792214b25e02571af5294665a9a6bac7ad
Hua <hualyu.2018@gmail.com>
2022-09-13 12:19:36 +0930

    message

--- main.rb
+++ main.rb
@@ -1,11 +1,10 @@
      
 require 'sinatra'
-require 'hattparty'
+require 'httparty'
 require 'dotenv/load'
 
 get '/' do
   # HTTParty.get("http://omdbapi.com?apikey=#{ENV['OMDB_API_KEY']}&t=jaws")
-  erb :index
 end
 
 
